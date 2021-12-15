4a01c0796350dea7990529381f4cbb3e05f4d37f
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2021-12-15 09:48:23 +0000

    dfx json changed sligthly
    ID was added to the dfx json so it would work with mongo db

--- server.js
+++ server.js
@@ -14,21 +14,20 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 const main = async () => {
-	// console.log(`Connecting to DB @ ${process.env.DB_URI}`);
-	// await mongoose.connect("http://localhost:27017/dfx");
-	await mongoose.connect(process.env.DB_URI);
+  // console.log(`Connecting to DB @ ${process.env.DB_URI}`);
+  await mongoose.connect(process.env.DB_URI);
 };
 
 app.use(`/loginPage`, loginPage);
 app.use(`/trainee`, trainee);
 
 main()
-	.then(() => console.log(`Connecting to DB @ ${process.env.DB_URI}`))
-	.catch(err => console.log(err));
+  .then(() => console.log(`Connecting to DB @ ${process.env.DB_URI}`))
+  .catch((err) => console.log(err));
 
 const server = app.listen(port, host, () => {
-	console.log("Server started");
-	const SERVERHOST = server.address().address;
-	const SERVERPORT = server.address().port;
-	console.log(`Server is running on http://${SERVERHOST}:${SERVERPORT}`);
+  console.log("Server started");
+  const SERVERHOST = server.address().address;
+  const SERVERPORT = server.address().port;
+  console.log(`Server is running on http://${SERVERHOST}:${SERVERPORT}`);
 });
