3145a389ee90e3182d8cdba8e7d19fbd64f6e582
RaposoJoshlan <91786311+RaposoJoshlan@users.noreply.github.com>
2022-01-05 22:29:12 +0000

    for login

---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { emailRegExp, passwordRegex } = require(`../jsRegex/regEx`);
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: [true, `No email supplied`],
+		match: [emailRegExp, `Invalid Email format`]
+	},
+	password: {
+		type: String,
+		required: [true, "No password supplied"],
+		validator: value => {
+			return value.length > 4;
+		},
+		message: "password not long enough. Should be greater than 4 characters",
+		match: [passwordRegex, `password doesn't meet requirements`]
+	},
+	roles: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Role"
+            }
+        ]
+});
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
