54469d4ac94acf3beb7133fefa8077588b9e086a
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2022-01-11 12:23:24 +0000

    Update singleTrainee.js

--- routes/singleTrainee.js
+++ routes/singleTrainee.js
@@ -6,15 +6,18 @@ const Trainees = require("../models/trainee.model");
 router.use(express.json());
 
 router.route(`/:email`).get((req, res) => {
-	const { email } = req.body;
-
-	Trainees.findOne({ email }, (err, user) => {
-		if (!user) {
-			res.status(404).send("Not working");
-		} else {
-			res.json(user);
-		}
-	});
+  const { email } = req.params;
+  console.log(req.params);
+  Trainees.findOne(
+    { "personalDetails.contact.email.workEmail": email },
+    (err, user) => {
+      if (!user) {
+        res.status(404).send("Not working");
+      } else {
+        res.json(user);
+      }
+    }
+  );
 });
 
 module.exports = router;
